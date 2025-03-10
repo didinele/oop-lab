4ff514d911f8145f2d6606d7dcdd4f2cd5807cf8
didinele <didinele.dev@gmail.com>
2025-03-10 17:35:59 +0200

    lab 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,75 @@
-#include "thing.hpp"
+#include <iostream>
+#include <string>
 
-int main() {
-    do_someting();
-    return 0;
+class A
+{
+  private:
+    std::string m_Status;
+
+  public:
+    A()
+    {
+        std::cout << "created A\n";
+    }
+
+    virtual ~A()
+    {
+        std::cout << "destroyed A\n";
+    }
+
+    void SetStatus(std::string status)
+    {
+        m_Status = status;
+    }
+
+    std::string GetStatus()
+    {
+        return m_Status;
+    }
+};
+
+class B : public A
+{
+  public:
+    B()
+    {
+        std::cout << "created B\n";
+    }
+    ~B()
+    {
+        std::cout << "destroyed B\n";
+    }
+};
+
+class Printable {
+    virtual void DrawMe() = 0;
+};
+
+class Moveable {
+    virtual void Move() = 0;
+};
+
+class Shape : public Printable, public Moveable {
+
+};
+
+class Square : public Shape {
+
+};
+
+int main()
+{
+    A* a_ptr = new A();
+    a_ptr->SetStatus("abc");
+
+    A* b_ptr = new B();
+    b_ptr->SetStatus("def");
+
+    std::cout << a_ptr->GetStatus() << '\n';
+    std::cout << b_ptr->GetStatus() << '\n';
+
+    delete a_ptr;
+    delete b_ptr;
+
+    Square square;
 }
